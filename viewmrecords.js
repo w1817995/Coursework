aa97690e4dbd5fb2bf45044e82fc09d315b0d0d5
w1817995 <128593627+w1817995@users.noreply.github.com>
2023-05-03 15:28:50 +0100

    Add files via upload

--- viewmrecords.js
+++ viewmrecords.js
@@ -1,11 +1,9 @@
 import {Button, Page,Footer, LabelText,Link, Table} from "govuk-react";
 import React, { useState,useEffect } from 'react';
-import { useLocation } from "react-router-dom";
 import $ from 'jquery';
 
 function ViewMR(props) {
   const [data, setData] = useState('');
-  const location = useLocation();
   const[auth,setAuth] = useState('');
 
   useEffect(() => {
@@ -32,6 +30,81 @@ function ViewMR(props) {
       </Page>
     );
   }  
+
+      const records = [];
+
+        for (let i = 0; i < data.length; i++) {
+        const record = data[i];
+        records.push(
+                <div key={record.vaccinationDate}>
+                  <br />
+                  <h2>Vaccination Date: {record.vaccinationDate}</h2>
+                  <Table>
+                    <Table.Row>
+                      <Table.CellHeader>DoseNum</Table.CellHeader>
+                      <Table.Cell>{record.DoseNum}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                      <Table.CellHeader>vaccinationDate</Table.CellHeader>
+                      <Table.Cell>{record.vaccinationDate}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                      <Table.CellHeader>vaccineManufacturer</Table.CellHeader>
+                      <Table.Cell>{record.vaccineManufacturer}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                      <Table.CellHeader>diseaseTargeted</Table.CellHeader>
+                      <Table.Cell>{record.diseaseTargeted}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                      <Table.CellHeader>vaccineType</Table.CellHeader>
+                      <Table.Cell>{record.vaccineType}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                      <Table.CellHeader>product</Table.CellHeader>
+                      <Table.Cell>{record.product}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                      <Table.CellHeader>vaccineBatchNumber</Table.CellHeader>
+                      <Table.Cell>{record.vaccineBatchNumber}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                      <Table.CellHeader>countryOfVaccination</Table.CellHeader>
+                      <Table.Cell>{record.countryOfVaccination}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                      <Table.CellHeader>authority</Table.CellHeader>
+                      <Table.Cell>{record.authority}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                      <Table.CellHeader>totalSeriesOfDoses</Table.CellHeader>
+                      <Table.Cell>{record.totalSeriesOfDoses}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                      <Table.CellHeader>displayName</Table.CellHeader>
+                      <Table.Cell>{record.displayName}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                      <Table.CellHeader>snomedCode</Table.CellHeader>
+                      <Table.Cell>{record.snomedCode}</Table.Cell>
+                    </Table.Row>
+                    <Table.Row>
+                     <Table.CellHeader>dateEntered</Table.CellHeader>
+                      <Table.Cell>{record.dateEntered}</Table.Cell>
+                     </Table.Row>
+                     <Table.Row>
+                     <Table.CellHeader>procedureCode</Table.CellHeader>
+                     <Table.Cell>{record.procedureCode}</Table.Cell>
+                     </Table.Row>
+                     <Table.Row>
+                     <Table.CellHeader>booster</Table.CellHeader>
+                     <Table.Cell>{record.booster}</Table.Cell>
+                    </Table.Row>
+                    </Table>
+          </div>
+          )
+        }
+
      return (
     <Page>
       <LabelText><h1><center>View Medical Records Page</center></h1></LabelText>
@@ -41,74 +114,7 @@ function ViewMR(props) {
       <div>
         <p>Your NHS number is: {auth}</p>
        {
-        data.map(record => (
-           <div key={record.vaccinationDate}>
-            <br/>
-            <h2>Vaccination Date: {record.vaccinationDate}</h2>
-            <Table>
-          <Table.Row>
-            <Table.CellHeader>DoseNum</Table.CellHeader>
-            <Table.Cell>{record.DoseNum}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>vaccinationDate</Table.CellHeader>
-            <Table.Cell>{record.vaccinationDate}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>vaccineManufacturer</Table.CellHeader>
-            <Table.Cell>{record.vaccineManufacturer}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>diseaseTargeted</Table.CellHeader>
-            <Table.Cell>{record.diseaseTargeted}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>vaccineType</Table.CellHeader>
-            <Table.Cell>{record.vaccineType}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>product</Table.CellHeader>
-            <Table.Cell>{record.product}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>vaccineBatchNumber</Table.CellHeader>
-            <Table.Cell>{record.vaccineBatchNumber}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>countryOfVaccination</Table.CellHeader>
-            <Table.Cell>{record.countryOfVaccination}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>authority</Table.CellHeader>
-            <Table.Cell>{record.authority}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>totalSeriesOfDoses</Table.CellHeader>
-            <Table.Cell>{record.totalSeriesOfDoses}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>displayName</Table.CellHeader>
-            <Table.Cell>{record.displayName}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>snomedCode</Table.CellHeader>
-            <Table.Cell>{record.snomedCode}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>dateEntered</Table.CellHeader>
-            <Table.Cell>{record.dateEntered}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>procedureCode</Table.CellHeader>
-            <Table.Cell>{record.procedureCode}</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.CellHeader>booster</Table.CellHeader>
-            <Table.Cell>{record.booster}</Table.Cell>
-          </Table.Row>
-          </Table>
-           </div>
-        ))
+         data.map(() => (records))
        }
 </div>
     <br/>
