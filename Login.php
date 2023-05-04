85351435d140685ada9c8c16ab87799461fe02ac
w1817995 <128593627+w1817995@users.noreply.github.com>
2023-05-04 14:05:56 +0100

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,4 +1,5 @@
 <?php
+//Fareed Khan Mohamed Rafi did this page
 header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, Content-Type, Accept");
