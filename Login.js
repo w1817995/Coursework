5d8b4077b98fbf47ed00728bb5366b55ae513604
w1817995 <128593627+w1817995@users.noreply.github.com>
2023-05-04 14:00:24 +0100

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,3 +1,4 @@
+//Elis morina did the all the front end of this page, Fareed khan mohamed rafi did the whole back end and Saad fasial helped all around this page.
 import React, { useState } from "react";
 import { Button, Input, LabelText, Page } from "govuk-react";
 import { Link } from "react-router-dom";
