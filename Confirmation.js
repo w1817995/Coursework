2c699cea5d88a94f2eb0c8bf2edeae5356011ea8
w1817995 <128593627+w1817995@users.noreply.github.com>
2023-05-05 17:40:26 +0100

    Add files via upload

--- Confirmation.js
+++ Confirmation.js
@@ -1,3 +1,4 @@
+//Fareed Khan Mohamed Rafi did this whole page
 import { Panel,Page, Button,Link } from "govuk-react";
 import React from "react";
 const Confirmation = () =>{
@@ -9,6 +10,7 @@ return(
      <Link href="Login">
      <Button>Login</Button>
      </Link>
+     <br/>
      <Link>
      <Button>De-Register</Button>
      </Link>
